46c86bc1bb64874220d54c5ff1cbe1763ad32e21
Devin Cornell <devinj.cornell@gmail.com>
2017-02-15 12:53:09 -0800

    Fixed a bug with detecting http error codes. Should work now.

--- nyt.py
+++ nyt.py
@@ -8,8 +8,11 @@ class NYT:
 
 	def __init__(self):
 		self.conn = http.client.HTTPConnection("api.nytimes.com")
-		with open('config.txt','r') as f: #is config.txt a file that needs to be downloaded?
-			self.cfg = json.load(f)
+		try:
+			with open('config.json','r') as f: #is config.txt a file that needs to be downloaded?
+				self.cfg = json.load(f)
+		except:
+			raise(Exception('Error loading config.json file. Does it exist and is it json?'))
 
 	def get_archive(self, year=1851, month=12):
 		''' Either gets previously retreived data or makes a new
@@ -27,22 +30,27 @@ class NYT:
 
 
 	def archives_api_request(self, year, month):
+		
+		# make request to api using get variable "api-key"
 		self.conn.request("GET", "//svc/archive/v1/" + str(year) + \
 		"/" + str(month) + ".json?api-key=%s" % (self.cfg['apikey'],))
+		
+		# get response from connection object
 		r = self.conn.getresponse()
 		
-		if r.status is not 200:
-			raise(Exception('Query to NYT failed. Did you insert your api key in config.txt?'))
+		# detect the http error codes - if there was an error it was probably the api key
+		if r.status is 403:
+			raise(Exception('Api key was rejected. Did you paste it in config.json?'))
+		elif r.status is not 200:
+			raise(Exception('Error making request to nyt.com.'))
 		
 		return json.loads(r.read().decode("ascii"))
-
-#d = json.loads(r.read().decode("ascii"))
-
+	
 
 if __name__ == "__main__":
 	nytapi = NYT()
 
-	d = nytapi.get_archive(year=1851, month=12)
+	d = nytapi.get_archive(year=1853, month=12)
 
 	print(d)
 	for doc in d['response']['docs']:
