cfa17ef81c4e7ac9eb54dd7c1f5638cd8481dcf0
iggytrinh <iggytrinh@yahoo.com>
2017-02-22 20:55:21 -0800

    developed more functions to capture headline data from NYTimes API and put them into strings

--- textAnalysis.py
+++ textAnalysis.py
@@ -5,19 +5,21 @@
 
 from nyt import *
 
-if __name__ == "__main__":
-	nytapi = NYT()
-
-	d = nytapi.get_archive(year=1853, month=12)
-
+##if __name__ == "__main__":
+##	nytapi = NYT()
+##
+##	d = nytapi.get_archive(year=1853, month=12)
+##
 ##	for doc in d['response']['docs']:
 ##		print(doc['headline']['main'])
 
 
-def createHeadlineString(archive):
-##      Function which returns a string comprised of all the headlines of an archive(from a
-##      given year, month)
-        
+def createHeadlineString(year = 1853, month = 12):
+##      -Function which returns a string comprised of all the headlines of an archive(from a
+##       given year, month)
+        nytapi = NYT()
+        d = nytapi.get_archive(year, month)
+
         headlineString = ""
 
         for doc in d['response']['docs']:
@@ -29,3 +31,32 @@ def createHeadlineString(archive):
                         headlineString += doc['headline']['main']
 
         return headlineString
+
+##Understand TF IDF
+##Create Title Strings from entire decade.
+
+def createYearHeadlineString(year = 1853):
+##      -Function which returns a string comprised of all of the headlines of the archives from
+##       a given year.
+        yearHeadlineString = ""
+
+        for month in range(1, 13):
+                yearHeadlineString += " " + createHeadlineString(year, month)
+
+        return yearHeadlineString
+
+
+def createYearRangeHeadlineString(startYear = 1853, endYear = 1862):
+##      -Function which returns a string comprised of all of the headlines of the archives from
+##       a given range of years.
+        
+        yearRangeHeadlineString = ""
+        
+        for year in range(startYear, endYear + 1):
+                yearRangeHeadlineString += " " + createYearHeadlineString(year)
+
+        return yearRangeHeadlineString
+
+
+
+
