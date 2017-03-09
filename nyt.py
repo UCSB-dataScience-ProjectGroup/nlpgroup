f6e1a31a48c5ee5b4ffe8a7c900e9c1d41628d26
Devin Cornell <devinj.cornell@gmail.com>
2017-03-08 20:34:52 -0800

    Made feature to use local copy of data if it has been requested before. Also made adjustable config file, adjustable data folder, and added a tester that takes command line arguments.

--- nyt.py
+++ nyt.py
@@ -1,18 +1,24 @@
 import http.client
 import json
-
+import os
 
 
 
 class NYT:
 
-	def __init__(self):
+	def __init__(self,config_file='config.json',data_folder='nytapi_data/'):
 		self.conn = http.client.HTTPConnection("api.nytimes.com")
 		try:
-			with open('config.json','r') as f: #is config.txt a file that needs to be downloaded?
+			with open(config_file,'r') as f: #is config.txt a file that needs to be downloaded?
 				self.cfg = json.load(f)
 		except:
 			raise(Exception('Error loading config.json file. Does it exist and is it json?'))
+
+		# data folder
+		self.data_folder = data_folder
+		#print(os.path.isfile('nytapi_data'))
+		if not os.path.isdir(data_folder):
+			os.mkdir(data_folder)
 
 	def get_archive(self, year=1851, month=12):
 		''' Either gets previously retreived data or makes a new
@@ -20,17 +26,24 @@ class NYT:
 		year and month.
 		'''
 
-		## for now, this downloads new data every time
-
-		## in the future, store results in a file and if the data
-		## 	is requested again then just use the stored data. It
-		##  would be reasonable to make a new file for each 
-		##  year/month combination.
-		json_string = self.archives_api_request(year,month)
+		fname = self.data_folder + '{}.{}.json'.format(year,month)
+		if os.path.isfile(fname):
+			json_string = self.archives_local(year,month,fname)
+		else:
+			json_string = self.archives_apirequest(year,month)
+			with open(fname,'w') as f:
+				f.write(json_string)
+		
 		return json.loads(json_string)
+	
+	def archives_local(self, year, month, fname):
+		# get data from local file
+		with open(fname,'r') as f:
+			json_string = f.read()
+		return json_string
 
 
-	def archives_api_request(self, year, month):
+	def archives_apirequest(self, year, month):
 		
 		# make request to api using get variable "api-key"
 		self.conn.request("GET", "//svc/archive/v1/" + str(year) + \
@@ -49,10 +62,25 @@ class NYT:
 	
 
 if __name__ == "__main__":
+	import sys
+
+	if len(sys.argv) > 1:
+		yr = str(sys.argv[1])
+		mo = str(sys.argv[2])
+	else:
+		yr = 1853
+		mo = 12
+
+	# initiate nytapi library
 	nytapi = NYT()
+	
+	# make a request to get the archive
+	d = nytapi.get_archive(year=yr, month=mo)
 
-	d = nytapi.get_archive(year=1853, month=12)
-
-	print(d)
+	
+	i = 1
 	for doc in d['response']['docs']:
 		print(doc['headline']['main'])
+		if i > 10:
+			break
+		i += 1
