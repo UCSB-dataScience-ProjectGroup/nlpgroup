b43b76f9448beab211a0e038eaac85ca1ed894b9
Devin Cornell <devinj.cornell@gmail.com>
2017-04-22 16:57:05 -0700

    Adjusted num words.

--- yearcloud.py
+++ yearcloud.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 	print('hello world')
 	nytapi = NYT()
-	wordcloud = WordCloud()
+	wordcloud = WordCloud(collocations=False)
 
 	# use each month as a doc
 	yrz = [(1853,12),(1853,11),(1853,10)]
@@ -21,15 +21,18 @@ if __name__ == '__main__':
 	
 	# print number of tokens used in the tfidf of each doc
 	i = 1
+	import numpy as np
 	for doc in scores:
 		fstr = []
+		#scz = 1000*np.array(list(doc.values()))
+		#print(np.histogram(scz))
 		for w in doc.keys():
-			fstr += [w,]*int(100*doc[w])
+			fstr += [w,]*int(1000*doc[w])
 		wc = wordcloud.generate(' '.join(fstr))
 		#plt.imshow(wc)
 		plt.imsave(str(i)+'.png',wc)
 
-		#print(len(doc.keys()))
+		print(str(i), 'has', len(doc.keys()), 'different words.')
 		i += 1
 
 
