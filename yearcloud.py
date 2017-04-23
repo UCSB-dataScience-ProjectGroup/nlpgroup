b51d0474421310d9a0ef4a555089d1462cf6c94e
Devin Cornell <devinj.cornell@gmail.com>
2017-04-22 17:36:27 -0700

    Fixed encoding issues.

--- yearcloud.py
+++ yearcloud.py
@@ -5,16 +5,27 @@ import tfidflib
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+from os import listdir
+from os.path import isfile, join
+
+
+def readf(fname):
+	with open(fname,'rb') as f:
+		r = f.read().decode('utf-8', errors='replace')
+	return r
 
 if __name__ == '__main__':
-	print('hello world')
+	mypath = 'data/'
+
 	nytapi = NYT()
 	wordcloud = WordCloud(collocations=False)
 
 	# use each month as a doc
-	yrz = [(1853,12),(1853,11),(1853,10)]
-	docs = [' '.join([doc['headline']['main'] for doc in nytapi.get_archive(*yrmo)['response']['docs']]) for yrmo in yrz]
-
+	#yrz = [(1853,12),(1853,11),(1853,10)]
+	#docs = [' '.join([doc['headline']['main'] for doc in nytapi.get_archive(*yrmo)['response']['docs']]) for yrmo in yrz]
+	files = [str(f) for f in listdir(mypath) if isfile(join(mypath, str(f))) and f[0] != '.']
+	#print(listdir(mypath))
+	docs = [readf(join(mypath,fname)) for fname in files]
 
 	# get tfidf scores from each doc
 	scores = tfidflib.getscores(docs)
@@ -30,7 +41,7 @@ if __name__ == '__main__':
 			fstr += [w,]*int(1000*doc[w])
 		wc = wordcloud.generate(' '.join(fstr))
 		#plt.imshow(wc)
-		plt.imsave(str(i)+'.png',wc)
+		plt.imsave(files[i] + '.png',wc)
 
 		print(str(i), 'has', len(doc.keys()), 'different words.')
 		i += 1
